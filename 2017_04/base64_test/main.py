6e04bcc84dd33b6bc4b93fa06fe1cb0b4e289c55
TonyMooori <tony.mooori@gmail.com>
2017-04-26 22:39:11 +0900

    studying about base64
    The image data in ipython notebook is saved as base64. This program
    decodes the encoded data to the original image data.

---
+++ 2017_04/base64_test/main.py
@@ -0,0 +1,15 @@
+﻿#conding:uft-8
+
+import base64
+
+# ipython notebookから引っ張ってきたpng画像をbase64形式で保存したデータ
+with open("./pngdata.txt","r") as f:
+    png_base64 = f.readlines()
+
+png_base64 = "".join(png_base64)
+print(png_base64)
+
+dec_file = base64.b64decode( png_base64 )
+
+with open('./output.png', 'wb') as f:
+    f.write(dec_file)
